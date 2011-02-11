b7fba17717a2ec9e97b38795186387f99299ee92
Joe Jones <jjones@wallickcos.com>
2011-02-11 00:16:25 -0500

    Testing of resizing list and status label.

--- ToDo.Net/fMain.Designer.cs
+++ ToDo.Net/fMain.Designer.cs
@@ -29,57 +29,45 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.statusStrip = new System.Windows.Forms.StatusStrip();
-            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
-            this.statusStrip.SuspendLayout();
+            this.lStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 14;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Location = new System.Drawing.Point(61, 153);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(260, 368);
-            this.listBox1.TabIndex = 0;
+            this.listBox1.Size = new System.Drawing.Size(163, 102);
+            this.listBox1.TabIndex = 2;
             // 
-            // statusStrip
+            // lStatus
             // 
-            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.statusLabel});
-            this.statusStrip.Location = new System.Drawing.Point(0, 387);
-            this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(284, 22);
-            this.statusStrip.TabIndex = 1;
-            // 
-            // statusLabel
-            // 
-            this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(118, 17);
-            this.statusLabel.Text = "toolStripStatusLabel1";
+            this.lStatus.Location = new System.Drawing.Point(100, 310);
+            this.lStatus.Name = "lStatus";
+            this.lStatus.Size = new System.Drawing.Size(35, 13);
+            this.lStatus.TabIndex = 3;
+            this.lStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // fMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 409);
-            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.lStatus);
             this.Controls.Add(this.listBox1);
             this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "fMain";
             this.Text = "ToDo.Net";
-            this.statusStrip.ResumeLayout(false);
-            this.statusStrip.PerformLayout();
+            this.Resize += new System.EventHandler(this.fMain_Resize);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.StatusStrip statusStrip;
-        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.Label lStatus;
     }
 }
 
--- ToDo.Net/fMain.cs
+++ ToDo.Net/fMain.cs
@@ -14,6 +14,28 @@ namespace ToDo.Net
         public fMain()
         {
             InitializeComponent();
+            ResizeListing();
+        }
+
+        private void fMain_Resize(object sender, EventArgs e)
+        {
+            ResizeListing();
+        }
+
+        private void ResizeListing()
+        {
+            listBox1.Top = 0;
+            listBox1.Left = 0;
+            listBox1.Width = this.ClientSize.Width;
+            listBox1.Height = this.ClientSize.Height;
+
+            // position the status label.
+            lStatus.Top = this.ClientSize.Height - 30;
+            lStatus.Left = 0;
+            lStatus.Width = this.ClientSize.Width;
+            lStatus.Height = 20;
+            lStatus.Text = "test";
+            lStatus.BringToFront();
         }
     }
 }
